762578209112faafc374b65ffa9f87ca31e63df9
sameermaner <106797445+sameermaner@users.noreply.github.com>
2024-09-11 20:27:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
 provider "azurerm" {
-skip_provider_registration = "true"
-subscription_id = var.subscription_id
-client_id = var.client_id
-client_secret = var.client_secret
-tenant_id = var.tenant_id
     features {}
 }
 resource "azurerm_resource_group" "rg1" {
